65b7abdc87a882b6ec55127737058048f4fe8ea3
游騰信 <s1081647@gm.pu.edu.tw>
2021-05-21 14:52:02 +0800

    2021上機考v0_游騰信

---
+++ app/src/main/java/tw/edu/pu/s1081647/crazyshape/MainActivity.kt
@@ -0,0 +1,21 @@
+package tw.edu.pu.s1081647.crazyshape
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(baseContext, "作者：游騰信", Toast.LENGTH_LONG).show()
+
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                intent = Intent(this@MainActivity, GameActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        })
+    }
+}
